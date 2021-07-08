32bf014ab9e6176945d23ed26c08409dcdaba69c
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-07-09 06:16:04 +0800

    completed initial API file

--- src/api/api.js
+++ src/api/api.js
@@ -8,7 +8,7 @@ export const registerUser = ({ email, password, password_confirmation }) => {
     password_confirmation
   })
   .then(response => response.text())
-  .then(result => console.log("User Registration Result:", result))
+  .then(result => result)
   .catch(error => console.log("User Registration Error:", error))
 }
 
@@ -19,19 +19,145 @@ export const userLogin = ({ email, password }) => {
     password
   })
   .then(response => response)
-  .then(result => console.log("Login Result:", result))
+  .then(result => result)
   .catch(error => console.log("Login Error:", error))
 }
 
 //send message
-export const sendMessage = ({ receiver_id, receiver_class, body }) => {
-  axios.post("http://206.189.91.54//api/v1/messages", {
+export const sendMessage = ({ receiver_id, receiver_class, body, headers:{ token, client, expiry, uid } }) => {
+  axios.post(
+    "http://206.189.91.54//api/v1/messages", 
+  {
     receiver_id,
     receiver_class,
     body
+  },
+  {
+    headers:{
+      "access-token": token,
+      "client": client,
+      "expiry": expiry,
+      "uid": uid,
+    }
   })
   .then(response => response.text())
   .then(result => console.log("Send Message Result:", result))
   .catch(error => console.log("Send Message Error:", error))
 }
 
+//get messages
+export const getMessage = ({ receiver_id, receiver_class, headers:{ token, client, expiry, uid } }) => {
+  axios.get(
+    "http://206.189.91.54//api/v1/messages",
+    {
+      headers:{
+        "access-token": token,
+        "client": client,
+        "expiry": expiry,
+        "uid": uid,
+      },
+      params: {
+        receiver_id,
+        receiver_class
+      }
+    })
+    .then(response => response.text())
+    .then(result => result)
+    .catch(error => console.log("Get Message Error:", error))
+}
+
+//create channel
+export const createChannel = ({ name, user_ids, headers:{ token, client, expiry, uid } }) => {
+  axios.post(
+    "http://206.189.91.54//api/v1/channels", 
+  {
+    name,
+    user_ids
+  },
+  {
+    headers:{
+      "access-token": token,
+      "client": client,
+      "expiry": expiry,
+      "uid": uid,
+    }
+  })
+  .then(response => response.text())
+  .then(result => console.log("Create Channel Result:", result))
+  .catch(error => console.log("Create Channel Error:", error))
+}
+
+//get all channels
+export const getChannels = ({ token, client, expiry, uid }) => {
+  axios.get(
+    "http://206.189.91.54//api/v1/channels",
+    {
+      headers:{
+        "access-token": token,
+        "client": client,
+        "expiry": expiry,
+        "uid": uid,
+      }
+    })
+    .then(response => response.text())
+    .then(result => result)
+    .catch(error => console.log("Get Message Error:", error))
+}
+
+//get channel details
+export const getChannelDetail = ({ id, headers:{ token, client, expiry, uid } }) => {
+  axios.get(
+    "http://206.189.91.54//api/v1/channels",
+    {
+      headers:{
+        "access-token": token,
+        "client": client,
+        "expiry": expiry,
+        "uid": uid,
+      },
+      params:{
+        "id": id //channel id
+      }
+    })
+    .then(response => response.text())
+    .then(result => result)
+    .catch(error => console.log("Get Message Error:", error))
+}
+
+//add user to channel
+export const addMemberToChannel = ({ id, member_id, headers:{ token, client, expiry, uid } }) => {
+  axios.post(
+    "http://206.189.91.54//api/v1/channel/add_member", 
+  {
+    id,
+    member_id
+  },
+  {
+    headers:{
+      "access-token": token,
+      "client": client,
+      "expiry": expiry,
+      "uid": uid,
+    }
+  })
+  .then(response => response.text())
+  .then(result => console.log("Add Member To Channel Result:", result))
+  .catch(error => console.log("Add Member To Channel Error:", error))
+}
+
+//get all users
+export const getAllUsers = ({ token, client, expiry, uid }) => {
+  axios.get(
+    "http://206.189.91.54//api/v1/users",
+    {
+      headers:{
+        "access-token": token,
+        "client": client,
+        "expiry": expiry,
+        "uid": uid,
+      }
+    })
+    .then(response => response.text())
+    .then(result => result)
+    .catch(error => console.log("Get Message Error:", error))
+}
