d993bfad8cd60549ee548564a91049078e4f9b1a
stephdajon <stephdajon@gmail.com>
2021-07-09 17:18:16 +0800

    STEPH: HEADER

--- src/components/Header/HeaderHelp.js
+++ src/components/Header/HeaderHelp.js
@@ -2,8 +2,8 @@ import "../Header/HeaderHelp.scss"
 
 const HelpBtn = () => {
     return (
-        <div className="header-help-btn">
-            <button><i class="far fa-question-circle"></i></button>
+        <div className="header-help">
+            <button className="header-help-btn"><i class="far fa-question-circle"></i></button>
         </div>
     )
 }
--- src/components/Header/HeaderSearch.js
+++ src/components/Header/HeaderSearch.js
@@ -2,8 +2,8 @@ import "../Header/HeaderSearch.scss"
 
 const SearchBar = () => {
     return (
-        <div className="header-search-bar">
-            <button>Search Avion School <i class="fas fa-search"></i></button>
+        <div className="header-search">
+            <button className="header-search-btn">Search Avion School <i class="fas fa-search"></i></button>
         </div>
     )
 }
