b3cd0b07a6642e0caf79502e63afead08789564c
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-07-14 03:43:19 +0800

    setup chat item component to use given props to render details

--- src/components/Chat/ChatItem/ChatItem.js
+++ src/components/Chat/ChatItem/ChatItem.js
@@ -1,10 +1,27 @@
 import "./ChatItem.scss"
 
-const ChatItem = () => {
-
+const ChatItem = ({id, name, date, body}) => {
+  id = 5;
+  name = "Sample Name"
+  date = "9:17 AM"
+  body =  "Sample Message Sample Messsage Hellow Hi."
   return(
     <div className="chat_container-item">
-      Item
+      <div className="chat_container-contents">
+        <section className="chat_container-img">
+          <img src={`https://picsum.photos/id/${id}/40`} /> 
+        </section>
+        <section className="chat_container-details">
+          <div className="chat_details-name">
+            <h1>{ name }</h1>
+            <label>{ date }</label>
+          </div>
+          <div className="chat_details-body">
+            { body }
+          </div>
+        </section>
+      
+      </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import ApiTestPage from './ApiTestPage';
 import App from './App';
 import "./index.scss"
+import Main from "./pages/Main"
 
 ReactDOM.render(
     // <ApiTestPage />, //use this if you want to test API
-    <App />,
+    <Main />,
   document.getElementById('root')
 );
