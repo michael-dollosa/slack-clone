7415c3772a797646c456cb6dc3a8363d782c7923
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-07-15 22:33:08 +0800

    fixed overall app UI

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,29 @@
 import "../Header/Header.scss";
-import HelpBtn from "./HeaderHelp";
-import History from "./HeaderHistory";
-import SearchBar from "./HeaderSearch";
-import HeaderUser from "./HeaderUser";
+import { MdAccessTime } from "react-icons/md";
+import { FiSearch } from "react-icons/fi";
+import { IoIosHelpCircleOutline } from "react-icons/io";
 
-const Header = () => {
-    return (
-        <div className="header-container">
-            <History />
-            <SearchBar />
-            <HelpBtn />
-            <HeaderUser />
+const Header = ({userID}) => {
+
+    return(
+        <div className="header-container ">
+            <section className="header-history header-flex-center">
+                <MdAccessTime className="header-side-icons"/>
+            </section>
+
+            <section className="header-search">
+                <button className="header-search-btn">
+                    <FiSearch className="icon"/>
+                    <span>Search Avion School</span> 
+                </button>
+            </section>
+
+            <section className="header-help header-flex-center">
+                <IoIosHelpCircleOutline className="header-side-icons"/>
+            </section>
+            <section className="header-user-img">
+                <img src={`https://picsum.photos/id/${userID}/26`} />
+            </section>
         </div>
     )
 }
