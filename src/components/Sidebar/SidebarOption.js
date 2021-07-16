d80c9cdf01a531ab89c21f47ed9f9de2f9cd660d
bryanarboledaa <80822041+bryanarboledaa@users.noreply.github.com>
2021-07-16 12:32:04 +0800

    BRYAN: addChannel UI and form toggle

--- src/components/Sidebar/SidebarOption.js
+++ src/components/Sidebar/SidebarOption.js
@@ -8,44 +8,24 @@ const SidebarOption = ({
   handleAddChannelToggle = null,
   optionType,
 }) => {
-  
   return (
     <section className="sidebar-option-item">
-      {
-        optionType
-        ? optionType === "user"
-          ? 
-            <div className="sidebar-option sidebar-padding">
-              <section className="sidebar-option-user-img sidebar-option-flex-center">
-                <img src={Icon} alt="" />
-              </section>
-              {Icon 
-                ? (
-                  <h3 onClick={handleAddChannelToggle}>{title}</h3>
-                  ) 
-                : (
-                  <h3 className="sidebar-option-channel sidebar-option-flex-center">
-                    <span className="sidebar-option-hash">#</span>
-                  </h3>
-                  )
-                }
-            </div>
-          : 
+      {optionType ? (
+        optionType === "user" ? (
           <div className="sidebar-option sidebar-padding">
-            {Icon && <Icon className="sidebar-option-icon sidebar-option-flex-center" />}
-            {Icon 
-              ? (
-                <h3 onClick={handleAddChannelToggle}>{title}</h3>
-                ) 
-              : (
-                <h3 className="sidebar-option-channel sidebar-option-flex-center">
-                  <span className="sidebar-option-hash">#</span>
-                </h3>
-                )
-              }
+            <section className="sidebar-option-user-img sidebar-option-flex-center">
+              <img src={Icon} alt="" />
+            </section>
+            {Icon ? (
+              <h3 onClick={handleAddChannelToggle}>{title}</h3>
+            ) : (
+              <h3 className="sidebar-option-channel sidebar-option-flex-center">
+                <span className="sidebar-option-hash">#</span>
+              </h3>
+            )}
           </div>
         ) : (
-          <div className="sidebar-option">
+          <div className="sidebar-option sidebar-padding">
             {Icon && (
               <Icon className="sidebar-option-icon sidebar-option-flex-center" />
             )}
